ca9d4facd3f8b8b86554801091f128db823223cc
YLori <yloritech@gmail.com>
2025-04-23 05:07:54 +0530

    uodate

--- app/events/[eventId]/edit/page.tsx
+++ app/events/[eventId]/edit/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from 'next/link';
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/app/lib/auth";
