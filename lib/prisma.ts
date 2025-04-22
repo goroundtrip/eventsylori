eb603574498e52ccce696f80eb3e3643466bd439
goroundtrip <goroundtrip1@gmail.com>
2025-04-22 05:53:23 +0530

    Fix: include creator.email in admin event update query

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,18 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
 
-const globalForPrisma = global as unknown as {
+const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
+    log: ['query'], // You can remove this in production if you prefer
   })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma 
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
+
+// Export Prisma namespace too, for enums like Prisma.QueryMode
+export { Prisma }
